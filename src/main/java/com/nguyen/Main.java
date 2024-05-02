4f299568400cc936705035d1a3d2660bb9d4ffa2
Nguyen H. K. Bui <nguyenbui1996@gmail.com>
2024-05-02 06:32:29 -0700

    add wait

--- src/main/java/com/nguyen/Main.java
+++ src/main/java/com/nguyen/Main.java
@@ -5,12 +5,14 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class Main {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://chat.openai.com/");
-        driver.manage().deleteAllCookies();
         driver.findElement(By.xpath("//*[@id=\"prompt-textarea\"]")).sendKeys("what is the most popular transportation in Vietnam?");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         driver.findElement(By.xpath("//*[@id=\"prompt-textarea\"]")).sendKeys(Keys.RETURN);
 
     }
